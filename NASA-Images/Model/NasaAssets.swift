67e53189c99a21a7c2840de5630d160075fcc662
iyinraphael <iyinraphael@gmail.com>
2021-03-13 14:00:49 -0800

    created codable model

---
+++ NASA-Images/Model/NasaAssets.swift
@@ -0,0 +1,34 @@
+//
+//  NasaAssets.swift
+//  NASA-Images
+//
+//  Created by Iyin Raphael on 3/13/21.
+//
+
+import Foundation
+
+
+struct NasaAssets: Codable {
+    let collection: CollectionAsset
+    
+    struct CollectionAsset: Codable {
+        let items: [ItemAsset]
+        
+        struct ItemAsset: Codable {
+            let data: [DataAsset]
+            let links: [LinkAsset]
+            
+            struct DataAsset: Codable {
+                let title: String
+                let photographer: String
+                let location: String
+                let description: String
+            }
+            struct LinkAsset: Codable {
+                let href: String
+            }
+        }
+    }
+}
+
+
---
+++ NASA-Images/Network/Network.swift
@@ -0,0 +1,14 @@
+//
+//  Network.swift
+//  NASA-Images
+//
+//  Created by Iyin Raphael on 3/13/21.
+//
+
+import Foundation
+
+class Network {
+    
+    
+    
+}
