df80e0df7abbac16a5c35d593e17633114b35d0d
iyinraphael <iyinraphael@gmail.com>
2021-03-14 00:08:55 -0800

    customized appearance

---
+++ NASA-Images/Utility/Appearance.swift
@@ -0,0 +1,15 @@
+//
+//  Appearance.swift
+//  NASA-Images
+//
+//  Created by Iyin Raphael on 3/13/21.
+//
+
+import UIKit
+
+enum Appearance {
+    static let fontColor = UIColor(red: 74/255, green: 74/255, blue: 74/255, alpha: 1)
+    static let blueColor = UIColor(red: 0/255, green: 147/255, blue: 175/255, alpha: 1)
+    static let valueFont = UIFont(name: "AvenirNext-DemiBold", size: 12)
+    static let labelFont = UIFont(name: "AvenirNext-Regular", size: 11)
+}
